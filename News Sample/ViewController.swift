de9bfa72e08bf6e93d40a2f710bf7b73ada627df
meksconways <muhammedeminkisi@gmail.com>
2023-03-19 01:27:37 +0300

    Initial Commit

---
+++ News Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Sample
+//
+//  Created by Muhammed Emin Kişi on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'News Sample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for News Sample
+  pod 'Moya/RxSwift', '~> 15.0'
+  pod 'RxCocoa', '6.2.0'
+
+end
